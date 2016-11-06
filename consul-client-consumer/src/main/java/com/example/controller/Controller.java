cd0ea2351267e4dae5924aa5084e03b844247398
Engin Yöyen <engin.yoyen@gmail.com>
2016-11-06 22:15:37 +0100

    Add resource to retrieve all service instances

--- consul-client-consumer/src/main/java/com/example/controller/Controller.java
+++ consul-client-consumer/src/main/java/com/example/controller/Controller.java
@@ -2,14 +2,27 @@ package com.example.controller;
 
 import com.example.model.Recommendation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
 public class Controller {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{serviceName}")
+    public List<ServiceInstance> getServiceInstances(@PathVariable String serviceName){
+        return discoveryClient.getInstances(serviceName);
+    }
 
     @Bean
     @LoadBalanced
